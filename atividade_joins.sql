ef5eb96046592df3922b668d785e83d4205fce9b
alessandro <alessandrotibia664@gmail.com>
2025-09-18 08:45:11 -0300

    Add files via upload

---
+++ atividade_joins.sql
@@ -0,0 +1,28 @@
+SELECT v.id_venda, v.data, c.nome_cliente, c.cidade, p.nome_produto, v.qtd_produto, p.preco_unit, v.qtd_produto * p.preco_unit AS valor_total
+FROM produtos AS p
+INNER JOIN vendas AS v
+ON p.id_produto = v.id_produto
+INNER JOIN clientes AS c
+ON v.id_cliente = c.id_cliente
+ORDER BY v.qtd_produto * p.preco_unit DESC, id_venda DESC
+;
+
+SELECT p.categoria, SUM(p.preco_unit * v.qtd_produto) AS faturamento
+FROM produtos AS p
+INNER JOIN vendas AS v
+ON p.id_produto = v.id_produto
+GROUP BY p.categoria
+ORDER BY SUM(p.preco_unit * v.qtd_produto) DESC
+LIMIT 2
+;
+
+SELECT c.nome_cliente, SUM(p.preco_unit * v.qtd_produto) AS gasto_total
+FROM clientes AS c
+INNER JOIN vendas AS v
+ON c.id_cliente = v.id_cliente
+INNER JOIN produtos AS p
+ON v.id_produto = p.id_produto
+WHERE categoria = "Computador"
+GROUP BY c.nome_cliente
+HAVING SUM(p.preco_unit * v.qtd_produto) >= 3000
+;
